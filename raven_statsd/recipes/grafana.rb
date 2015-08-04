ba4046ef1194781d1c8f535d9c9aab8a2f885a08
Phil Dier <phil@raventools.com>
2015-08-04 11:25:40 -0500

    update to grafana 2.1

--- raven_statsd/recipes/grafana.rb
+++ raven_statsd/recipes/grafana.rb
@@ -3,9 +3,9 @@ include_recipe "raven_statsd::default"
 
 package "fontconfig"
 
-grafana_rpm = "#{node[:raven_statsd][:tmp_dir]}/grafana-2.0.2-1.x86_64.rpm"
+grafana_rpm = "#{node[:raven_statsd][:tmp_dir]}/grafana-2.1.0-1.x86_64.rpm"
 remote_file grafana_rpm do
-	source "https://grafanarel.s3.amazonaws.com/builds/grafana-2.0.2-1.x86_64.rpm"
+	source "https://grafanarel.s3.amazonaws.com/builds/grafana-2.1.0-1.x86_64.rpm"
 end
 
 rpm_package grafana_rpm
